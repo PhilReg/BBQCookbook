ca44c27ff30c6fafd4054af669a67bc199717f86
Phil <phr7536@thi.de>
2022-11-13 21:50:00 +0100

    Frontend setup 1

---
+++ WebApplication1/Controllers/KochvorgangController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BBQLibary;
+
+namespace WebApplication1.Controllers
+{
+    public class KochvorgangController : Controller
+    {
+        private Model1Container db = new Model1Container();
+
+        // GET: Kochvorgang
+        public ActionResult Index()
+        {
+            var kochvorgangSet = db.KochvorgangSet.Include(k => k.Rezept);
+            return View(kochvorgangSet.ToList());
+        }
+
+        // GET: Kochvorgang/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kochvorgang kochvorgang = db.KochvorgangSet.Find(id);
+            if (kochvorgang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kochvorgang);
+        }
+
+        // GET: Kochvorgang/Create
+        public ActionResult Create()
+        {
+            ViewBag.RezeptId = new SelectList(db.RezeptSet, "Id", "Zutatenliste");
+            return View();
+        }
+
+        // POST: Kochvorgang/Create
+        // Aktivieren Sie zum Schutz vor Angriffen durch Overposting die jeweiligen Eigenschaften, mit denen eine Bindung erfolgen soll. 
+        // Weitere Informationen finden Sie unter https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Protokoll,RezeptId")] Kochvorgang kochvorgang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.KochvorgangSet.Add(kochvorgang);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RezeptId = new SelectList(db.RezeptSet, "Id", "Zutatenliste", kochvorgang.RezeptId);
+            return View(kochvorgang);
+        }
+
+        // GET: Kochvorgang/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kochvorgang kochvorgang = db.KochvorgangSet.Find(id);
+            if (kochvorgang == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RezeptId = new SelectList(db.RezeptSet, "Id", "Zutatenliste", kochvorgang.RezeptId);
+            return View(kochvorgang);
+        }
+
+        // POST: Kochvorgang/Edit/5
+        // Aktivieren Sie zum Schutz vor Angriffen durch Overposting die jeweiligen Eigenschaften, mit denen eine Bindung erfolgen soll. 
+        // Weitere Informationen finden Sie unter https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Protokoll,RezeptId")] Kochvorgang kochvorgang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kochvorgang).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RezeptId = new SelectList(db.RezeptSet, "Id", "Zutatenliste", kochvorgang.RezeptId);
+            return View(kochvorgang);
+        }
+
+        // GET: Kochvorgang/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kochvorgang kochvorgang = db.KochvorgangSet.Find(id);
+            if (kochvorgang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kochvorgang);
+        }
+
+        // POST: Kochvorgang/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Kochvorgang kochvorgang = db.KochvorgangSet.Find(id);
+            db.KochvorgangSet.Remove(kochvorgang);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/RezeptController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BBQLibary;
+
+namespace WebApplication1.Controllers
+{
+    public class RezeptController : Controller
+    {
+        private Model1Container db = new Model1Container();
+
+        // GET: Rezept
+        public ActionResult Index()
+        {
+            return View(db.RezeptSet.ToList());
+        }
+
+        // GET: Rezept/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rezept rezept = db.RezeptSet.Find(id);
+            if (rezept == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rezept);
+        }
+
+        // GET: Rezept/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Rezept/Create
+        // Aktivieren Sie zum Schutz vor Angriffen durch Overposting die jeweiligen Eigenschaften, mit denen eine Bindung erfolgen soll. 
+        // Weitere Informationen finden Sie unter https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Zutatenliste,Vorgehen,Holzart,Equipmentliste")] Rezept rezept)
+        {
+            if (ModelState.IsValid)
+            {
+                db.RezeptSet.Add(rezept);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rezept);
+        }
+
+        // GET: Rezept/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rezept rezept = db.RezeptSet.Find(id);
+            if (rezept == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rezept);
+        }
+
+        // POST: Rezept/Edit/5
+        // Aktivieren Sie zum Schutz vor Angriffen durch Overposting die jeweiligen Eigenschaften, mit denen eine Bindung erfolgen soll. 
+        // Weitere Informationen finden Sie unter https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Zutatenliste,Vorgehen,Holzart,Equipmentliste")] Rezept rezept)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rezept).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(rezept);
+        }
+
+        // GET: Rezept/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rezept rezept = db.RezeptSet.Find(id);
+            if (rezept == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rezept);
+        }
+
+        // POST: Rezept/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Rezept rezept = db.RezeptSet.Find(id);
+            db.RezeptSet.Remove(rezept);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
