0b10bf5eda16c29dc68fa7c966d8ecbac0df01b8
Phil <phr7536@thi.de>
2022-11-14 22:07:13 +0100

    Login Controller angelegt

---
+++ WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BBQLibary;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        private Model1Container db = new Model1Container();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View(db.KochSet.ToList());
+        }
+
+        // GET: Login/Details/5
+        public ActionResult Select(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Koch koch = db.KochSet.Find(id);
+            if (koch == null)
+            {
+                return HttpNotFound();
+            }
+            return Redirect("Index");
+        }
+    }
+}
