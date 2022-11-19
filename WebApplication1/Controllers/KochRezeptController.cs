6fd4ac780c9220a9f035cfaf8334e264f2633d1c
Phil <phr7536@thi.de>
2022-11-19 19:49:38 +0100

    Rezept zu Koch zuweisen angefangen

---
+++ WebApplication1/Controllers/KochRezeptController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ImpBBQLibary;
+
+namespace WebApplication1.Controllers
+{
+    public class KochRezeptController : BaseController
+    {
+        private Model1Container db = new Model1Container();
+
+        // GET: KochRezept
+        public ActionResult Index()
+        {
+            return View(db.RezeptSet.ToList());
+        }
+
+        // GET: KochRezept/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rezept rezept = db.RezeptSet.Find(id);
+            if (rezept == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rezept);
+        }
+
+        // GET: KochRezept/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: KochRezept/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Rezeptnamen,Vorgehen,Rezeptbewertung")] Rezept rezept)
+        {
+            if (ModelState.IsValid)
+            {
+                db.RezeptSet.Add(rezept);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rezept);
+        }
+
+        // GET: KochRezept/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rezept rezept = db.RezeptSet.Find(id);
+            if (rezept == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rezept);
+        }
+
+        // POST: KochRezept/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Rezeptnamen,Vorgehen,Rezeptbewertung")] Rezept rezept)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rezept).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(rezept);
+        }
+
+        // GET: KochRezept/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rezept rezept = db.RezeptSet.Find(id);
+            if (rezept == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rezept);
+        }
+
+        // POST: KochRezept/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Rezept rezept = db.RezeptSet.Find(id);
+            db.RezeptSet.Remove(rezept);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public ActionResult Rezpeterstellen(int? id)
+        {
+            if(id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rezept rezept = db.RezeptSet.Find(id);
+            if (rezept == null)
+            {
+                return HttpNotFound();
+            }
+            if (!hasUser())
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LoggedInKoch.Rezept.Add(rezept);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication1/Controllers/ZutatenlisteController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ImpBBQLibary;
+
+namespace WebApplication1.Controllers
+{
+    public class ZutatenlisteController : BaseController
+    {
+        private Model1Container db = new Model1Container();
+
+        // GET: Zutatenliste
+        public ActionResult Index()
+        {
+            return View(db.ZutatenSet.ToList());
+        }
+
+        // GET: Zutatenliste/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Zutaten zutaten = db.ZutatenSet.Find(id);
+            if (zutaten == null)
+            {
+                return HttpNotFound();
+            }
+            return View(zutaten);
+        }
+
+        // GET: Zutatenliste/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Zutatenliste/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Zutatennamen,Kategorie")] Zutaten zutaten)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ZutatenSet.Add(zutaten);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(zutaten);
+        }
+
+        // GET: Zutatenliste/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Zutaten zutaten = db.ZutatenSet.Find(id);
+            if (zutaten == null)
+            {
+                return HttpNotFound();
+            }
+            return View(zutaten);
+        }
+
+        // POST: Zutatenliste/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Zutatennamen,Kategorie")] Zutaten zutaten)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(zutaten).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(zutaten);
+        }
+
+        // GET: Zutatenliste/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Zutaten zutaten = db.ZutatenSet.Find(id);
+            if (zutaten == null)
+            {
+                return HttpNotFound();
+            }
+            return View(zutaten);
+        }
+
+        // POST: Zutatenliste/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Zutaten zutaten = db.ZutatenSet.Find(id);
+            db.ZutatenSet.Remove(zutaten);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
